f3047d991264b946244ac0966a39a63dc8acc917
akeum <ayoung.keum@students.olin.edu>
2014-06-03 23:15:51 +0900

    network with macro

--- src/network.h
+++ src/network.h
@@ -1,9 +1,21 @@
 #ifndef __NETWORK_H
 #define __NETWORK_H
 
-#define NETWORK_BASESTATION_CHANNEL     0x14
-#define NETWORK_BASESTATION_PAN_ID      0x0000
-#define NETWORK_BASESTATION_ADDR        0x0100
-#define NETWORK_SRC_ADDR                0x0110
+#if defined (__C2)
+	#define NETWORK_BASESTATION_CHANNEL     0x14
+	#define NETWORK_BASESTATION_PAN_ID      0x0000
+	#define NETWORK_BASESTATION_ADDR        0x0101
+	#define NETWORK_SRC_ADDR                0x0012
 
+#elif defined(__C5)
+	#define NETWORK_BASESTATION_CHANNEL     0x14
+	#define NETWORK_BASESTATION_PAN_ID      0x0000
+	#define NETWORK_BASESTATION_ADDR        0x0100
+	#define NETWORK_SRC_ADDR                0x0015
+	
+#else 
+	#define NETWORK_BASESTATION_CHANNEL     0x14
+	#define NETWORK_BASESTATION_PAN_ID      0x0000
+	#define NETWORK_BASESTATION_ADDR        0x0100
+	#define NETWORK_SRC_ADDR                0x0110
 #endif
