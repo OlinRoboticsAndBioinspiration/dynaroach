c2ed59f237b1592822f6ba327f46d43d2b42e9c5
akeum <ayoung.keum@students.olin.edu>
2014-06-17 04:00:46 +0900

    added wii

--- src/cmd.c
+++ src/cmd.c
@@ -858,14 +858,16 @@ void motor_falling_edge() {
 }
 
 void cmdWiiDump(unsigned char status, unsigned char length, unsigned char* frame){
-    unsigned char wii_data[12] = {};  
+    unsigned char wii_data[12] = {'a','b','c','d'};  
     int i;
-    
+    //WiiBlob *B;
+    wiiReadData();
+    //wiiGetData(B);
     MD_LED_1 =1;
 	wiiDumpData(wii_data);
 	MD_LED_1 =0;
 	delay_ms(100);
-    send(status, length, wii_data, CMD_WII_DUMP);
+    send(status, 12, wii_data, CMD_WII_DUMP);
     for(i=0; i<3; i++){
 		MD_LED_1 = ~MD_LED_1;
 	}
